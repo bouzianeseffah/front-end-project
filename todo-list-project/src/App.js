2c8d57627563757743b45e38fff655a43e2cf2f7
Angel Soto <100176750+Angeltheesoto@users.noreply.github.com>
2022-09-10 14:45:21 -0400

    Fixed bootstrap to project

--- todo-list-project/src/App.js
+++ todo-list-project/src/App.js
@@ -1,7 +1,15 @@
+// DEPENDENCIES
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// COMPONENTS
+import Navbar from "./components/Navbar";
 
 function App() {
+
   return (
     <div className="App">
+      <Navbar />
       <h1>Starter code is </h1>
     </div>
   );
