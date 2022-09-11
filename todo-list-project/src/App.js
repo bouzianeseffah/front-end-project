0be34da61ff2ed57ca856ffbf907ad8ca927cb4f
Abdulaziz Ali <guuree1982@gmail.com>
2022-09-11 10:29:06 -0400

    added home page

--- todo-list-project/src/App.js
+++ todo-list-project/src/App.js
@@ -1,7 +1,12 @@
+import homePage from './Components/home'
+import productPage from './Components/product'
 
 function App() {
   return (
     <div className="App">
+      return(
+        
+      )
       <h1>Starter code is </h1>
     </div>
   );
