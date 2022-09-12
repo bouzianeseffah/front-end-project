e45927eec9cdbbd8199a2268ba1847719579fbb0
Angel Soto <100176750+Angeltheesoto@users.noreply.github.com>
2022-09-12 19:33:54 -0400

    Added username input on navbar

--- todo-list-project/src/components/Navbar.js
+++ todo-list-project/src/components/Navbar.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from '../images/Docket.png'
 
 function Nav() {
+
+  const [nameInForm, setNameInForm] = useState('')
+  const [show, setShow] = useState(true)
+
+  const inputName = (event) => {
+    setNameInForm(event.target.value);
+  }
+
+  // Show the user names input
+  function handleSubmit() {
+    setNameInForm(prevName => prevName = nameInForm)
+    // console.log('Welcome ' + nameInForm)
+    setShow(prevShow => prevShow === true ? false : true)
+    // console.log(show)
+  }
+
+  
   return (
     <div className="Nav">
       <Navbar bg="dark" variant="dark">
@@ -18,9 +35,41 @@ function Nav() {
             />{' '}
             Docket
           </Navbar.Brand>
+
           <Navbar.Brand >
-            Welcome, name
+            <form className={show ? 'show' : 'none'}>
+              <input 
+                type='text' 
+                id="firstname"
+                value={nameInForm}
+                placeholder="Add User.."
+                className="addName"
+                onChange={inputName}
+                maxLength='15'
+              />
+              <button 
+                type = "button" 
+                className="addName-button"
+                onClick={handleSubmit}
+                // onMouseDown={handleClose}
+              > 
+                <span>✔️</span>
+              </button>
+            </form>
+
+            <div className={show ? 'none' : 'show'}>
+              <span className="name">Welcome, {nameInForm} </span>
+              <button 
+                type = "button" 
+                className="addName-button"
+                onClick={handleSubmit}
+                // onMouseDown={handleShow}
+              >
+                <span>✏️</span>
+              </button>
+            </div>
           </Navbar.Brand>
+
           {/* Light dark mode button */}
           <Navbar.Brand href="#home">
             Light/Dark Mode
