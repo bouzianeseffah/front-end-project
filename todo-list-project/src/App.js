6d6f0fa6b902f3fdfd6ccf003b357ae847ff14e3
Abdulaziz Ali <guuree1982@gmail.com>
2022-09-12 21:01:50 -0400

    changed dark and light mode of this application

--- todo-list-project/src/App.js
+++ todo-list-project/src/App.js
@@ -1,39 +1,29 @@
 // DEPENDENCIES
 import React from 'react';
 import {useState} from 'react';
-// import Switch from 'react-input-switch';
 
 import  ReactDOM  from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // COMPONENTS
-import Nav from "./Components/Navbar";
-import Sidebar from "./Components/Sidebar";
-import Lists from "./Components/Lists";
-//New
-import {createContext} from "react";
-// import { BrowserRouter ,Link, Route  } from "react-router-dom";
-import ReactSwitch  from "react-switch";
-//Theme API
-export const ThemeContext = createContext('null')
+import Nav from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Lists from "./components/Lists";
+
 
 function App() {
-  const [theme, setTheme] = useState("light");
-  const toggleTheme = () =>{
-    setTheme((curr) => (curr === "light" ? "dark": "light"));
-  };
+
 
   return (
-    <ThemeContext.Provider value={{theme, toggleTheme}}> 
-      <div className="App" id={theme}>
-        <ReactSwitch />
-        <Nav />
+      <div className="App" >
+        <Nav 
+
+        />
         <div className="content-container">
           <Sidebar />
           <Lists />
         </div>
         </div>
-    </ThemeContext.Provider>
   );
 }
 
--- todo-list-project/src/components/Navbar.js
+++ todo-list-project/src/components/Navbar.js
@@ -1,38 +1,59 @@
-import React from "react";
+import React,{useState} from "react";
+
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from '../images/Docket.png'
+//New
+import {createContext} from "react";
+// import { BrowserRouter ,Link, Route  } from "react-router-dom";
+import ReactSwitch  from "react-switch";
+//Theme API
+const ThemeContext = createContext('null')
 
 function Nav() {
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = () =>{
+    setTheme((curr) => (curr === "light" ? "dark": "light"));
+  };
   return (
-    <div className="Nav">
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">
-            <img
-              alt=""
-              src={logo}
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{' '}
-            Docket
-          </Navbar.Brand>
-          <Navbar.Brand >
-            Welcome, name
-            <form>
-              <label>
-                <input type="text" placeholder="your name"/>
-              </label>
-            </form>
-          </Navbar.Brand>
-          {/* Light dark mode button */}
-          <Navbar.Brand href="#home">
-            Light/Dark Mode
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-    </div>
+    <ThemeContext.Provider value={{theme, toggleTheme}}> 
+      <div className="Nav">
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">
+              <img
+                alt=""
+                src={logo}
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+              />{' '}
+              Docket
+            </Navbar.Brand>
+            <Navbar.Brand >
+              Welcome, name
+              <form>
+                <label>
+                  <input type="text" placeholder="your name"/>
+                </label>
+              </form>
+
+            </Navbar.Brand>
+            {/* Light dark mode button */}
+          <Navbar.Brand href="#home" id={theme}>
+            <label>{theme === "light"? "light mode": "dark mode"}</label>
+            <ReactSwitch onChange={toggleTheme} checked={theme === "dark"}/>
+            <div className="switch"> 
+
+            </div>
+
+
+            </Navbar.Brand>
+          </Container>
+        </Navbar>
+         </div>
+    </ThemeContext.Provider>
+
   );
 }
 
