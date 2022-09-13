e1edb98c8a09af5e12979910e0ef999ce524bcf9
Angel Soto <100176750+Angeltheesoto@users.noreply.github.com>
2022-09-13 14:16:11 -0400

    Finished add button for folders

--- todo-list-project/src/components/Sidebar.js
+++ todo-list-project/src/components/Sidebar.js
@@ -8,9 +8,49 @@ import ArrowRight from '../images/arrow-right.svg'
 
 function Sidebar() {
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  
+  const [title, setTitle] = useState('')
+  const [allTitles, setAllTitles] = useState(['Note 1'])
+  
+  const handleClick = (e) => {
+    e.preventDefault()
+    // Clears input when entered
+    console.log(title)
+    setTitle(prevTitle => prevTitle = '')
+
+    // add notes button
+    setAllTitles(prevAllTitles => [title, ...allTitles])
+    console.log(allTitles)
+  }
+
+  // deletes current Folder
+  const deleteClick = () => {
+    // setAllTitles([...title].filter(currentId => currentId.id !== id))
+    console.log(allTitles.id)
+  }
+
+
+
+  const inputName = (event) => {
+    setTitle(event.target.value);
+  }
+  
+  const folderElements = allTitles.map((note, index) => (
+    <>
+      <p>
+        {note} 
+        {/* {index + 1} */}
+        <button className='sidebar-delete-buttons' onClick={deleteClick}>
+          Delete
+        </button>
+        <button className='sidebar-edit-buttons'>
+          Edit
+        </button>
+      </p>
+    </>
+  ))
 
   return (
     <div className='sidebar'>
@@ -26,16 +66,26 @@ function Sidebar() {
         <Offcanvas.Header closeButton>
           <Offcanvas.Title>Notes</Offcanvas.Title>
         </Offcanvas.Header>
-        <Offcanvas.Body >
-          <p className="mb-0">
-            This is content within a container.
-          </p>
-        </Offcanvas.Body>
-        <Offcanvas.Body>
-          <p className="mb-0">
-            This is content within a container 3.
-          </p>
-        </Offcanvas.Body>
+
+        <div className='folder-container'>
+          <form>
+          <input
+            value={title}
+            onChange={inputName}
+            maxLength='15'
+          />
+          <button 
+            type='button'
+            className='add-folder-title'
+            onClick={handleClick}
+          >Add</button>
+          </form>
+          <br/>
+
+          {folderElements}
+
+        </div>
+
       </Offcanvas>
     </div>
   );
