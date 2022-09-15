d426360cc68f4c7ed04723b7b4db94822bd7cdbf
Abdulaziz Ali <guuree1982@gmail.com>
2022-09-14 20:32:23 -0400

    position relative given to switch mode button

--- todo-list-project/src/App.js
+++ todo-list-project/src/App.js
@@ -17,7 +17,7 @@ export const ThemeContext = createContext('null')
 function App() {
     const [theme, setTheme] = useState("light");
   const toggleTheme = () =>{
-    setTheme((curr) => (curr === "light" ? "dark": "light"));
+    setTheme((curr) => (curr === "light" ? "darklight": "light"));
   };
 
   return (
