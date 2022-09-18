08e4111ba1fa294c2701d611a6f8f076010a4ac6
Angel Soto <100176750+Angeltheesoto@users.noreply.github.com>
2022-09-18 14:11:10 -0400

    Added edit and delete to lists component

--- todo-list-project/src/components/MyNotes/Lists.js
+++ todo-list-project/src/components/MyNotes/Lists.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Card from "react-bootstrap/Card";
 import axios from "axios";
+import data from "../data/data";
 
 export default function Lists() {
   // const [notes, setNotes] = useState([]);
@@ -14,28 +15,80 @@ export default function Lists() {
   //   fetchNotes();
   // }, []);
 
+  const [newData, setNewData] = useState(data);
+  const [showEdit, setShowEdit] = useState(false);
+
+  function editTitle(event) {
+    // console.log(inputName2())
+    setShowEdit((prevShowEdit) => (prevShowEdit === false ? true : false));
+    if (showEdit === false) {
+      console.log("editing", event, showEdit);
+    } else {
+      console.log("edited", event, showEdit);
+      // setAllTitles(prevTitles => prevTitles.map(prevTitles => {
+      //   return prevTitles.title === title.title
+      //   ? {...prevTitles, title: event}
+      //   : prevTitles;
+      // }))
+      // return allTitles;
+    }
+  }
+
+  // deletes current folder name
+  function deleteTitle(id) {
+    setNewData((prevData) => prevData.filter((note) => note._id !== id));
+    // console.log("value of id:", newData._id);
+  }
+
   return (
     <div className="lists-container">
-      <Card border="info" className="list-card">
-        {/* <Card.Header>Header</Card.Header> */}
-        <Card.Body>
-          <Card.Title>Info Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <br />
-      <Card border="info" className="list-card">
-        <Card.Body>
-          <Card.Title>Info Card Title 2</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
+      {data.map((note) => (
+        <div key={note._id}>
+          <Card border="info" className="list-card">
+            {/* <Card.Header>Header</Card.Header> */}
+            <Card.Body>
+              <Card.Title>
+                {note.title}{" "}
+                {/* <h1 style={{ display: "inline", float: "right" }}>Delete</h1> */}
+                <button
+                  type="button"
+                  className="sidebar-delete-buttons"
+                  onClick={() => deleteTitle(note._id)}
+                >
+                  Delete
+                </button>
+                {!showEdit ? (
+                  <button
+                    style={{
+                      display: "inline",
+                      float: "right",
+                    }}
+                    type="button"
+                    className="sidebar-edit-buttons"
+                    onClick={() => editTitle(note.title)}
+                  >
+                    Edit
+                  </button>
+                ) : (
+                  <button
+                    style={{
+                      display: "inline",
+                      float: "right",
+                    }}
+                    type="button"
+                    className="sidebar-edit-buttons green"
+                    onClick={() => editTitle(note.title)}
+                  >
+                    Confirm
+                  </button>
+                )}
+              </Card.Title>
+              <Card.Text>{note.content}</Card.Text>
+            </Card.Body>
+          </Card>
+          <br />
+        </div>
+      ))}
     </div>
   );
 }
---
+++ todo-list-project/src/components/data/data.js
@@ -0,0 +1,28 @@
+const notes = [
+  {
+    _id: "1",
+    title: "This is title one",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    _id: "2",
+    title: "This is title two",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    _id: "3",
+    title: "This is title three",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    _id: 4,
+    title: "This is title four",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+];
+
+module.exports = notes;
