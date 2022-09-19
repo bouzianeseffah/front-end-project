1bf3a1d68bdfa219570150a40b9b0d267364663f
Angel Soto <100176750+Angeltheesoto@users.noreply.github.com>
2022-09-19 19:39:42 -0400

    Organized code

--- todo-list-project/src/components/data/data.js
+++ todo-list-project/src/components/data/data.js
@@ -2,26 +2,34 @@ const notes = [
   {
     _id: "1",
     title: "This is title one",
-    content:
+    description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    createdOn: false
+    date: 
   },
   {
     _id: "2",
     title: "This is title two",
-    content:
+    description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    createdOn: false,
+    date: false
   },
   {
     _id: "3",
     title: "This is title three",
     content:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    createdOn: false,
+    date: false
   },
   {
     _id: 4,
     title: "This is title four",
-    content:
+    description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    createdOn: false,
+    date: false
   },
 ];
 
