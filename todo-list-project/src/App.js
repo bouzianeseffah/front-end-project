90f90e2bf114aedf7296f66f787d6abf2e3d171d
Abdulaziz Ali <guuree1982@gmail.com>
2022-09-24 08:44:16 -0400

    connecting it to backend

--- todo-list-project/src/App.js
+++ todo-list-project/src/App.js
@@ -4,7 +4,6 @@ import {useState} from 'react';
 import  ReactDOM  from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 // COMPONENTS
@@ -12,57 +11,41 @@ import Nav from "./components/Header/Navbar";
 import LandingPage from "./views/LandingPage/LandingPage";
 import MyNotes from "./views/MyNotes";
 
+
 // COMPONENTS
 import Sidebar from "../src/components/MyNotes/Sidebar"
 import Lists from "./components/MyNotes/Lists"
 import app from "./App.js"
 import Footer from './components/Footer/Footer'
-
-// import Lists from './components/MyNotes/Lists';
-// 
-// import { nanoid } from 'nanoid'
-// model.id = nanoid() 
-
-
-// function App() {
-//     const [theme, setTheme] = useState("light");
-//   const toggleTheme = () =>{
-//     setTheme((curr) => (curr === "light" ? "dark": "light"));
-//   };
-
-//   return (
-//     <ThemeContext.Provider value={{theme, toggleTheme}}> 
-//       <div className="App" id={theme} >
-//         <Nav 
-//         />
-//         <div className="switch"> 
-//             <label>{theme === "light"? "Light Mode": "Dark Mode"}</label>
-//             <ReactSwitch onChange={toggleTheme} checked={theme === "dark"}/>
-//         </div>
-//         <div className="content-container">
-//           <Sidebar />
-//           <Lists />
-//         </div>
-//       </div>
-//     </ThemeContext.Provider>
-//   )
-// }
-
+// New
+import {createContext} from "react";
+import ReactSwitch  from "react-switch";
+//Create Theme API
+export const ThemeContext = createContext('light')
 
 
 function App() {
-
+  const [theme, setTheme] = useState("dark");
+  const toggleTheme = () =>{
+    setTheme((curr) =>(curr === "light" ? "dark": "light"));
+  };
   return (
-    <div className='App' >
-      <Router >
+    <ThemeContext.Provider value={{theme, toggleTheme}}> 
+      <div className="switch"> 
+        <label>{theme === "light"? "Light Mode": "Dark Mode"}</label>
+      <ReactSwitch onChange={toggleTheme} checked={theme === "dark"}/>
+      </div>
+      <div className='App' id={theme}>
+        <Router >
           <Nav />
           <Routes >
             <Route path="/" exact element={<LandingPage /> }></Route>
             <Route path="/mynotes" element={<MyNotes />}></Route>
           </Routes>
           <Footer />
-      </Router>
-    </div>
+        </Router>
+      </div>
+    </ThemeContext.Provider> 
 
   );
 }
--- todo-list-project/src/components/Header/Navbar.js
+++ todo-list-project/src/components/Header/Navbar.js
@@ -6,20 +6,13 @@ import { Link, Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
-// New
-import {createContext} from "react";
-import ReactSwitch  from "react-switch";
 
-//Create Theme API
-export const ThemeContext = createContext('light')
+
 
 function Nav() {
   const [nameInForm, setNameInForm] = useState("");
   const [show, setShow] = useState(true);
-  const [theme, setTheme] = useState("dark");
-  const toggleTheme = () =>{
-    setTheme((curr) =>(curr === "light" ? "dark": "light"));
-  };
+
 
   const inputName = (event) => {
     setNameInForm(event.target.value);
@@ -34,80 +27,75 @@ function Nav() {
   }
 
   return (
-    <ThemeContext.Provider value={{theme, toggleTheme}}> 
-      <div className="Nav" id={theme}>
-        <div className="switch"> 
-          <label>{theme === "light"? "Light Mode": "Dark Mode"}</label>
-          <ReactSwitch onChange={toggleTheme} checked={theme === "dark"}/>
+    
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand>
+          <img
+            alt=""
+            src={logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{" "}
+          <Link to="/" className="navbar-text-style">
+            Docket
+          </Link>
+        </Navbar.Brand>
+
+        <Navbar.Brand>
+          <form className={show ? "show" : "none"}>
+            <input
+              type="text"
+              id="firstname"
+              value={nameInForm}
+              placeholder="Add User.."
+              className="addName"
+              onChange={inputName}
+              maxLength="15"
+            />
+            <button
+              type="button"
+              className="addName-button"
+              onClick={handleSubmit}
+              // onMouseDown={handleClose}
+            >
+              <span role="img" aria-label="check mark">
+                ✔️
+              </span>
+            </button>
+          </form>
+
+          <div className={show ? "none" : "show"}>
+            <span className="name">Welcome, {nameInForm} </span>
+            <button
+              type="button"
+              className="addName-button"
+              onClick={handleSubmit}
+              // onMouseDown={handleShow}
+            >
+              <span role="img" aria-label="pencil image">
+                ✏️
+              </span>
+            </button>
+          </div>
+        </Navbar.Brand>
+        <div >
+          <Link
+            to="/mynotes"
+            className="navbar-text-style"
+            style={{ margin: "0 10px 0 0" }}
+          >
+            Notes
+          </Link>
+          
+          {/* Light dark mode button */}
+          <Navbar.Brand></Navbar.Brand>
         </div>
-        <Navbar bg="dark" variant="dark">
-          <Container>
-            <Navbar.Brand>
-              <img
-                alt=""
-                src={logo}
-                width="30"
-                height="30"
-                className="d-inline-block align-top"
-              />{" "}
-              <Link to="/" className="navbar-text-style">
-                Docket
-              </Link>
-            </Navbar.Brand>
+      </Container>
+    </Navbar> 
+      
 
-            <Navbar.Brand>
-              <form className={show ? "show" : "none"}>
-                <input
-                  type="text"
-                  id="firstname"
-                  value={nameInForm}
-                  placeholder="Add User.."
-                  className="addName"
-                  onChange={inputName}
-                  maxLength="15"
-                />
-                <button
-                  type="button"
-                  className="addName-button"
-                  onClick={handleSubmit}
-                  // onMouseDown={handleClose}
-                >
-                  <span role="img" aria-label="check mark">
-                    ✔️
-                  </span>
-                </button>
-              </form>
-
-              <div className={show ? "none" : "show"}>
-                <span className="name">Welcome, {nameInForm} </span>
-                <button
-                  type="button"
-                  className="addName-button"
-                  onClick={handleSubmit}
-                  // onMouseDown={handleShow}
-                >
-                  <span role="img" aria-label="pencil image">
-                    ✏️
-                  </span>
-                </button>
-              </div>
-            </Navbar.Brand>
-            <div>
-              <Link
-                to="/mynotes"
-                className="navbar-text-style"
-                style={{ margin: "0 10px 0 0" }}
-              >
-                Notes
-              </Link>
-              
-              {/* Light dark mode button */}
-              <Navbar.Brand></Navbar.Brand>
-            </div>
-          </Container>
-        </Navbar>
-      </div>
-    </ThemeContext.Provider> 
 
   );
 }
